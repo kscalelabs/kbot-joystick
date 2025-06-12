a811649c6e75b08bb994b72883ae6bd54d3821f9
b-vm <bart@vmeps.com>
2025-06-12 00:43:49 -0700

    setup script

---
+++ setup_env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Download Miniconda installer
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+
+# Install Miniconda
+bash miniconda.sh -b -p $HOME/miniconda
+
+# Initialize conda for bash
+eval "$($HOME/miniconda/bin/conda shell.bash hook)"
+
+conda create --name ksim --file conda-spec-file.txt
+conda activate ksim
+pip install -r requirements.txt
+
+
+echo "Environment setup complete! To activate the environment, run:"
+echo "conda activate ksim"
