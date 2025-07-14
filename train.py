1bfb3e9e243b1d6db99a6f361daca986734df145
b-x-ubuntu <bart@vmeps.com>
2025-07-14 12:51:26 -0700

    clean up

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ class HumanoidWalkingTaskConfig(ksim.PPOConfig):
     )
     use_gyro: bool = xax.field(
         value=True,
-        help="Whether to use the IMU acceleration and gyroscope observations.",
+        help="Whether to use the IMU gyroscope observations.",
     )
     # Optimizer parameters.
     learning_rate: float = xax.field(
@@ -1520,11 +1520,10 @@ if __name__ == "__main__":
             ctrl_dt=0.02,
             iterations=8,
             ls_iterations=8,
+            # sim2real parameters.
             action_latency_range=(0.003, 0.01),  # Simulate 3-10ms of latency.
             drop_action_prob=0.05,  # Drop 5% of commands.
             # Visualization parameters.
-            render_track_body_id=0,
-            render_markers=True,
             render_full_every_n_seconds=0,
             render_length_seconds=10,
             max_values_per_plot=50,
